41f4afcbf280e72294de298e6d1a92d1fd960170
Odilon Rafanambinantsoa <odilon.madasoftware@gmail.com>
2018-10-15 15:35:20 +0300

    add module url and query

--- myfirst.js
+++ myfirst.js
@@ -4,6 +4,7 @@ var dt = require('./dateModule');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write("The date and time are currently: " + dt.myDateTime());
+	//res.write(req.url);
     res.end('Hello World!');
 	
 }).listen(8080);
